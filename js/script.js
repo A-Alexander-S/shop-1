9f0289864d921e1af7309bde5e3548ad74081006
Alexander <impoossible.777@gmail.com>
2021-02-14 18:17:14 +0400

    адаптив

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+
+let buttonBurger = document.getElementById('burger');
+let navigation = document.getElementById('navigation');
+let introOverlay = document.querySelector('.intro__overlay');
+
+buttonBurger.addEventListener('click', function () {
+    if (document.querySelector('.intro') == null) {
+        navigation.classList.toggle('display-block');
+    } else {
+        navigation.classList.toggle('display-block');
+        introOverlay.classList.toggle('display-block');
+    }
+});
+
+let closeNavigation = document.querySelector('.navigation__button-cross');
+
+closeNavigation.addEventListener('click', function () {
+    if (document.querySelector('.intro') == null) {
+        navigation.classList.toggle('display-block');
+    } else {
+        navigation.classList.toggle('display-block');
+        introOverlay.classList.toggle('display-block');
+    }
+});
+
+let searchImg = document.getElementById('search-img');
+let headerSearch = document.getElementById('header-search');
+
+searchImg.addEventListener('click', function () {
+    headerSearch.classList.toggle('display-block');
+});
